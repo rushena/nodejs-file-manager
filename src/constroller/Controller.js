9881618f3430ff204f9b682d5b86f956af39b7b3
Rushen Adjigeldieva <rushena2706@gmail.com>
2024-02-05 17:34:55 +0500

    feat: add structure commands and his Controllers. Parse input value

--- src/constroller/Controller.js
+++ src/constroller/Controller.js
@@ -1,5 +1,39 @@
+import OsController from '../osController/OsController.js';
+import CompressController from '../compressController/CompressController.js';
+import FileController from '../fileController/FileController.js';
+import HashController from '../hashController/HashController.js';
+import NavigationController from '../navigationController/NavigationController.js';
+
 export default class Controller {
+  constructor() {
+    this.osController = new OsController();
+    this.compressController = new CompressController();
+    this.fileController = new FileController();
+    this.hashController = new HashController();
+    this.navigationController = new NavigationController();
+
+    this.commandsHandler = {
+      'navigationController': ['up', 'cd', 'ls'],
+      'fileController': ['cat', 'add', 'rn', 'cp', 'mv', 'rm'],
+      'osController': ['os'],
+      'compressController': ['compress', 'decompress'],
+      'hashController': ['hash']
+    }
+
+    this.currentPath = '';
+  }
+
   startHandler(value) {
-    console.log(value);
+    const valueArr = value.split(' ');
+    const command = valueArr.shift();
+
+    Object.entries(this.commandsHandler).forEach(([key, val]) => {
+      console.log(key, val);
+      console.log(command, valueArr);
+    })
+  }
+
+  normalizePath() {
+
   }
 }
