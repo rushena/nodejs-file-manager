7f91886f4e904796f55baa518136a36514d069a0
Rushen Adjigeldieva <rushena2706@gmail.com>
2024-02-05 15:45:26 +0500

    feat: add starting and exit message with username value

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 export class App {
   constructor() {
+    this.USER_NAME = this.getUserName();
+  }
+  start() {
+    console.log(`Welcome to the File Manager, ${this.USER_NAME}!`);
+    /*process.stdin.on('data', this.onGetStdinData);*/
+
+    process.on('SIGINT', () => {
+      this.exitProcess();
+    });
+  }
+
+  exitProcess() {
+    process.stdout.write(`\nThank you for using File Manager, ${this.USER_NAME}, goodbye!\n`);
+    process.exit(0);
+  }
+
+  getUserName() {
+    const startCliParams = process.argv.slice(2).filter((param) => param.startsWith('--username'));
+    return startCliParams[0]?.split('=')[1] || 'Username';
   }
 }
 
